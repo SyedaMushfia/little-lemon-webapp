48c4950278c267a304ea4591340e958e2dba0c4b
unknown <syedahshifa@ymail.com>
2025-02-18 14:02:32 +0530

    Set up the initial structure of homepage

---
+++ src/components/Footer.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <nav>
+        <ul>
+            <li>Doormat Navigation</li>
+            <li><a>Home</a></li>
+            <li><a>About</a></li>
+            <li><a>Menu</a></li>
+            <li><a>Reservations</a></li>
+            <li><a>Order Online</a></li>
+            <li><a>Login</a></li>
+        </ul>
+        <ul>
+            <li>Contact</li>
+            <li>Address</li>
+            <li>Phone Number</li>
+            <li>Email</li>
+        </ul>
+        <ul>
+            <li>Social Media Links</li>
+            <li>Address</li>
+            <li>Phone Number</li>
+            <li>Email</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Nav from './Nav'
+
+function Header() {
+  return (
+    <>
+      <div>
+      <img src='../../images/logo.jpg' alt='little-lemon-logo'/>
+    </div>
+    <Nav />
+    </>
+  )
+}
+
+export default Header
