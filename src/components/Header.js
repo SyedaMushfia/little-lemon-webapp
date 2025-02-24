25980214d996b94df7f3684003d887d401a9021f
unknown <syedahshifa@ymail.com>
2025-02-24 10:28:50 +0530

    Design a responsive header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,49 @@
 import React from 'react'
-import Nav from './Nav'
+import styles from "./header.module.css"
+import useViewportWidth from '../hooks/useViewportWidth'
 
 function Header() {
+  const width = useViewportWidth();
+
+  const isDesktop = width > 1024;
+  const isTab = width > 425 && width <= 1024;
+  const isMobile = width <= 425; 
+
   return (
     <>
-      <div>
-      <img src='../../images/logo.jpg' alt='little-lemon-logo'/>
-    </div>
-    <Nav />
+      <header className={styles.container}>
+        {isDesktop && (
+          <div className={styles.navbar}>
+            <img src='../../images/logo.jpg' alt='little-lemon-logo' className={styles.logo}/>
+            <nav>
+              <ul>
+                <li><a>Home</a></li>
+                <li><a>About</a></li>
+                <li><a>Menu</a></li>
+                <li><a>Reservations</a></li>
+                <li><a>Order Online</a></li>
+                <li><a>Login</a></li>
+              </ul>
+            </nav>
+          </div>
+        )}
+        {isTab && (
+          <div className={styles.navbar}>
+            <img src='../../images/logo.jpg' alt='little-lemon-logo' className={styles.logo}/>
+            <div className={styles.tabNavbar}>
+                <img src='../../images/🦆 icon _hamburger menu_.svg' alt='hamburger-menu'/>
+                <img src='../../images/basket .svg'/>
+            </div>
+          </div>
+        )}
+        {isMobile && (
+          <div className={styles.mobileNavbar}>
+            <img src='../../images/🦆 icon _hamburger menu_.svg' alt='hamburger-menu'/>
+            <img src='../../images/logo.jpg' alt='little-lemon-logo' className={styles.mobileLogo}/>
+            <img src='../../images/basket .svg'/>
+          </div>
+        )}
+      </header>
     </>
   )
 }
