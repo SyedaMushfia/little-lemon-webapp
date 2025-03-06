c61f8fc8202a6cbf1d742bf45e0dfb092b6a8466
unknown <syedahshifa@ymail.com>
2025-03-06 23:32:30 +0530

    Add the Routes component and define a route for each page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import { Routes, Route } from 'react-router';
+import Homepage from './homepage/Homepage';
+import AboutPage from './pages/AboutPage';
+import MenuPage from './pages/MenuPage';
+import BookingsPage from './pages/BookingsPage';
+import OrderOnline from './pages/OrderOnline';
+import LoginPage from './pages/LoginPage';
 
 function Main() {
   return (
-    <div>
-      
-    </div>
+    <main>
+    <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/menu" element={<MenuPage />} />
+        <Route path="/bookings" element={<BookingsPage />} />
+        <Route path="/orderonline" element={<OrderOnline />} />
+        <Route path="/login" element={<LoginPage />} />
+    </Routes>
+  </main>
   )
 }
 
