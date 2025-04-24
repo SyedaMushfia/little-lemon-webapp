778402766a396a97374c339d121958a7aef39cc6
unknown <syedahshifa@ymail.com>
2025-04-24 14:22:22 +0530

    Disable 'Next' buttons conditionally based on current step validation

--- src/components/pages/bookingPage/BookingForm.js
+++ src/components/pages/bookingPage/BookingForm.js
@@ -5,97 +5,139 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import { useState } from 'react';
-import { validateEmail, validatePhone } from '../../../utils';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import GroupsIcon from '@mui/icons-material/Groups';
 import CakeIcon from '@mui/icons-material/Cake';
 import DeckIcon from '@mui/icons-material/Deck';
 
-function BookingForm({availableTimes, formData, onDateChange, onInputChange, onClearForm, onSubmitForm}) {
-  const dropdownOptions = {
-    people: ['1 person', '2 people', '3 people', '4 people', '5 people'],
-    occasion: ['Birthday', 'Anniversary'],
-    seatingArea: ['Indoor', 'Outdoor'],
-  };
-
-  const showErrorMessage = {
-    firstName: "First name should be at least 2 characters",
-    lastName: "Last name should be at least 2 characters",
-    email: "Enter a valid email address",
-    phone: "Enter a valid phone number",
-  };
-
+function BookingForm({availableTimes, formData, errors, onInputBlur, onTimeFocus, onDateChange, onInputChange, onSubmitForm}) {
 
   const [step, setStep] = useState(1);
-  const [touched, setTouched] = useState({
-    firstName: false,
-    lastName: false,
-    email: false,
-    phone: false
-  })
 
-  const getIsFormValid = () => {
-    return (
-      formData.firstName?.trim() && 
-      formData.lastName?.trim() && 
-      formData.email?.trim() &&
-      formData.phone?.trim()
-    ); 
-  }; 
-  
-  const handleClickReserve = () => setStep(1);
-  const handleClickNext = () => {
-    if (step === 2) {
-      setTouched({
-        firstName: true,
-        lastName: true,
-        email: true,
-        phone: true
-      });
-  
-      if (!getIsFormValid()) return;
-
-      setTouched({
-        firstName: false,
-        lastName: false,
-        email: false,
-        phone: false
-      });
+  const stepButtons = [
+    {
+        step: 1,
+        name: 'RESERVE',
+        ariaLabel: 'Step 1: Reserve',
+        handleClick: () => setStep(1)
+    },
+    {
+        step: 2,
+        name: 'CONTACT',
+        ariaLabel: 'Step 2: Contact',
+        handleClick: () => setStep(2)
+    }, 
+    {
+        step: 3,
+        name: 'CONFIRM',
+        ariaLabel: 'Step 3: Confirm',
+        handleClick: () => setStep(3)
     }
+  ]
+
+  const selectFields = [
+    {
+        nameOfInput: 'Time',
+        id: 'time',
+        placeholder: '17:00',
+        error: 'time-error',
+        ariaLabel: 'Select reservation time',
+        options: availableTimes
+    },
+    {
+        nameOfInput: 'People',
+        id: 'people',
+        placeholder: '1 person',
+        error: 'people-error',
+        ariaLabel: 'Select number of people',
+        options: ['1 person', '2 people', '3 people', '4 people', '5 people']
+    },
+    {
+        nameOfInput: 'Occasion',
+        id: 'occasion',
+        placeholder: 'Birthday',
+        error: 'occasion-error',
+        ariaLabel: 'Select occasion',
+        options: ['Birthday', 'Anniversary']
+    },
+    {
+        nameOfInput: 'Seating Area',
+        id: 'seatingArea',
+        placeholder: 'Indoor',
+        error: 'seating-area-error',
+        ariaLabel: 'Select seating area',
+        options: ['Indoor', 'Outdoor']
+    }
+]
+
+const inputFields = [
+    {
+        nameOfInput: 'First Name',
+        id: 'firstName',
+        type: 'text',
+        minLength: 2,
+        ariaLabel: 'First Name',
+        ariaDescribedBy: 'first-name-error',
+    },
+    {
+        nameOfInput: 'Last Name',
+        id: 'lastName',
+        type: 'text',
+        minLength: 2,
+        ariaLabel: 'Last Name',
+        ariaDescribedBy: 'last-name-error',
+    },
+    {
+        nameOfInput: 'Email',
+        id: 'email',
+        type: 'email',
+        ariaLabel: 'Email',
+        ariaDescribedBy: 'email-error',
+    },
+    {
+        nameOfInput: 'Phone',
+        id: 'phone',
+        type: 'tel',
+        ariaLabel: 'Phone',
+        ariaDescribedBy: 'phone-error',
+    }
+]
+
+const isStepOneValid = formData.selectedDate &&
+selectFields.every(field => formData[field.id] && !errors[field.id]);
+
+const isStepTwoValid = inputFields.every(field => formData[field.id] && !errors[field.id]);
+
+const isNextDisabled = (step === 1 && !isStepOneValid) || (step === 2 && !isStepTwoValid);
+
   
-    setStep(prev => prev + 1);
+  const handleClickNext = () => {
+      setStep(prev => prev + 1);
   };
-  
-  const handleClickContact = () => setStep(2);
-  const handleClickConfirm = () => setStep(3);
     
-  const handleBlur = (e) => {
-    const {name} = e.target;
-
-    setTouched(prev => ({...prev, [name]: true}));
-  }
-
   const handleSubmit = (e) => {
     e.preventDefault();
-
     onSubmitForm(formData);
     console.log(formData);
   }
-
+  
   return (
     <>
     <div className={styles.container}>
         <div className={styles.formSteps}>
-            <div className={styles.formStepsBtns}>
-                <div>1</div>
-                <button type='button' role='button' onClick={handleClickReserve} aria-label="Step 1: Reserve">RESERVE</button></div>
-            <div className={styles.formStepsBtns}>
-                <div>2</div>
-                <button type='button' role='button' onClick={handleClickContact} aria-label="Step 2: Contact">CONTACT</button></div>
-            <div className={styles.formStepsBtns}>
-                <div>3</div>
-                <button type='button' role='button' onClick={handleClickConfirm} aria-label="Step 3: Confirm">CONFIRM</button></div>
+            {stepButtons.map(button => (
+                <div key={button.name} className={`${styles.formStepsBtns} ${step === button.step ? styles.activeStep : ''}`}>
+                    <div>{button.step}</div>
+                    <button 
+                        type='button' 
+                        role='button' 
+                        onClick={button.handleClick} 
+                        aria-label={button.ariaLabel}>
+                            {button.name}
+                            </button>
+                </div>    
+            ))}
         </div>
         <form onSubmit={handleSubmit}>
             {step === 1 && <>
@@ -106,145 +148,88 @@ function BookingForm({availableTimes, formData, onDateChange, onInputChange, onC
                         value={formData.selectedDate}
                         onChange={onDateChange}
                         aria-label="Select reservation date"
+                        disablePast
                         />
                     </LocalizationProvider>
+                    {errors.selectedDate && (
+                                <p className={styles.dateError} id="date-error" aria-live="assertive">{errors.selectedDate}</p>
+                )}
                 </div>
                 <div className={styles.formFieldsContainer}>
-                        <div>
-                            <label htmlFor="time">Time</label>
-                            <select 
-                                id="time" 
-                                value={formData['time']} 
-                                onChange={onInputChange} 
-                                aria-label="Select reservation time">
-                                {Array.isArray(availableTimes) && availableTimes.map(time => (
-                                    <option key={time} value={time}>{time}</option>
-                                ))}
-                            </select>
-                            <ArrowDropDownIcon className={styles.dropdownIcon}/>
-                        </div>
-                        <div>
-                            <label htmlFor="people">People</label>
+                    {selectFields.map(select => (
+                        <React.Fragment key={select.nameOfInput}>
+                            <div key={select.nameOfInput}>
+                            <label htmlFor={select.id}>{select.nameOfInput}</label>
                             <select
-                                id="people" 
-                                value={formData.people} 
-                                onChange={onInputChange} 
-                                aria-label="Select number of people">
-                                {dropdownOptions.people.map(number => (
-                                    <option key={number} value={number}>{number}</option>
+                                id={select.id}
+                                name={select.id}
+                                value={formData[select.id]}
+                                aria-label={select.ariaLabel}
+                                required
+                                aria-required="true"
+                                onBlur={onInputBlur}
+                                onChange={onInputChange}
+                                onFocus={select.id === 'time' ? onTimeFocus : null}
+                                className={`${errors[select.id] ? styles.errorInput : ''}`}
+                            >
+                                <option key={select.placeholder} value='' disabled hidden>{select.placeholder}</option>
+                                {select.options?.map((option, i) => (
+                                        <option key={option} value={option}>{option}</option>
                                 ))}
                             </select>
                             <ArrowDropDownIcon className={styles.dropdownIcon}/>
                         </div>
-                        <div>
-                            <label htmlFor="occasion">Occasion</label>
-                            <select
-                               id="occasion" 
-                               value={formData.occasion} 
-                               onChange={onInputChange} 
-                               aria-label="Select occasion">
-                                {dropdownOptions.occasion.map(event => (
-                                    <option key={event} value={event}>{event}</option>
-                                ))}
-                            </select>
-                            <ArrowDropDownIcon className={styles.dropdownIcon}/>
-                        </div>
-                        <div>
-                            <label htmlFor="seatingArea">Seating Area</label>
-                            <select 
-                                id="seatingArea" 
-                                value={formData.seatingArea} 
-                                onChange={onInputChange} 
-                                aria-label="Select seating area">
-                                {dropdownOptions.seatingArea.map(area => (
-                                    <option key={area} value={area}>{area}</option>
-                                ))}
-                            </select>
-                            <ArrowDropDownIcon className={styles.dropdownIcon}/>
-                        </div>
+                        {errors[select.id] && <p className={styles.errorMessage} id={select.error} aria-live="assertive">{errors[select.id]}</p>}
+                      </React.Fragment>
+                    ))}
                 </div>
             </div>
             <button 
-                className={styles.nextBtn} 
+                className={`${styles.nextBtn} ${isNextDisabled ? styles.disabledBtn : ''}`} 
                 onClick={handleClickNext} 
                 type='button' 
                 role='button'
                 aria-label="Proceed to Contact Information"
+                disabled={isNextDisabled}
                 >
                     Next</button>
             </>}
             {step === 2 && 
                 <>
                 <div className={styles.contactDetails}>
-                    <div>
-                        <label htmlFor='firstName'>First Name<span>*</span></label>
-                        <input 
-                            type='text' 
-                            id='firstName' 
-                            name='firstName' 
-                            value={formData.firstName} 
-                            onChange={onInputChange} 
-                            onBlur={handleBlur} 
-                            className={`${touched.firstName && formData.firstName.length < 2 ? styles.errorInput : ''}`} 
-                            aria-required="true" 
-                            aria-describedby="first-name-error"/>
-                    </div>
-                    {touched.firstName && formData.firstName.length < 2 ? <p id="first-name-error" aria-live="assertive" className={styles.errorMessage}>{showErrorMessage.firstName}</p> : null}
+                    {inputFields.map(field => (
+                        <React.Fragment key={field.nameOfInput}>
+                            <div key={field.nameOfInput}>
+                                <label htmlFor={field.id}>{field.nameOfInput}<span>*</span></label>
+                                <input 
+                                    type={field.type}
+                                    id={field.id}
+                                    name={field.id}
+                                    required
+                                    minLength={field.minLength}
+                                    value={formData[field.id]}
+                                    onChange={onInputChange}
+                                    onBlur={onInputBlur}
+                                    aria-required='true'
+                                    aria-label={field.ariaLabel}
+                                    aria-describedby={field.ariaDescribedBy}
+                                    aria-invalid={`${errors[field.id] ? 'true' : 'false'}`}
+                                    className={`${errors[field.id] ? styles.errorInput : ''}`} 
 
-                    <div>
-                        <label htmlFor='lastName'>Last Name<span>*</span></label>
-                        <input 
-                            type='text' 
-                            id='lastName' 
-                            name='lastName' 
-                            value={formData.lastName} 
-                            onChange={onInputChange} 
-                            onBlur={handleBlur} 
-                            className={`${touched.lastName && formData.lastName.length < 2 ? styles.errorInput : ''}`} 
-                            aria-required="true" 
-                            aria-describedby="last-name-error"/>
-                    </div>
-                    {touched.lastName && formData.lastName.length < 2 ? <p id="last-name-error" aria-live="assertive" className={styles.errorMessage}>{showErrorMessage.lastName}</p> : null}
+                                />
+                            </div>
+                            {errors[field.id] && <p id={field.ariaDescribedBy} aria-live="assertive" className={styles.errorMessage}>{errors[field.id]}</p>}
 
-                    <div>
-                        <label htmlFor='email'>Email<span>*</span></label>
-                        <input 
-                            type='email' 
-                            id='email' 
-                            name='email' 
-                            value={formData.email} 
-                            onChange={onInputChange} 
-                            onBlur={handleBlur} 
-                            className={`${touched.email && !validateEmail(formData.email) ? styles.errorInput : ''}`} 
-                            aria-required="true" 
-                            aria-describedby="email-error" 
-                            aria-invalid={touched.email && !validateEmail(formData.email) ? "true" : "false"}/>
-                    </div>
-                    {touched.email && !validateEmail(formData.email) ? <p id="email-error" aria-live="assertive" className={styles.errorMessage}>{showErrorMessage.email}</p> : null}
-
-                    <div>
-                        <label htmlFor='phone'>Phone<span>*</span></label>
-                        <input 
-                            type='tel' 
-                            id='phone' 
-                            name='phone' 
-                            value={formData.phone} 
-                            onChange={onInputChange} 
-                            onBlur={handleBlur} 
-                            className={`${touched.phone && !validatePhone(formData.phone) ? styles.errorInput : ''}`} 
-                            aria-required="true" 
-                            aria-describedby="phone-error" 
-                            aria-invalid={touched.phone && !validatePhone(formData.phone) ? "true" : "false"}/>
-                    </div>
-                    {touched.phone && !validatePhone(formData.phone) ? <p id="phone-error" aria-live="assertive" className={styles.errorMessage}>{showErrorMessage.phone}</p> : null}
-                    
+                        </React.Fragment>
+                    ))}
                 </div>
                 <button 
-                className={styles.nextBtn} 
+                className={`${styles.nextBtn} ${isNextDisabled ? styles.disabledBtn : ''}`} 
                 onClick={handleClickNext} 
                 type='button' 
                 role='button'
                 aria-label="Proceed to Confirm and Reserve"
+                disabled={isNextDisabled}
                 >
                     Next</button>
                 </>
@@ -256,7 +241,7 @@ function BookingForm({availableTimes, formData, onDateChange, onInputChange, onC
                     <div className={styles.lineBreak}>{/*Line Break*/}</div>
                     <div>
                         <CalendarMonthIcon />
-                        <p>{formData.selectedDate.format('MMMM D, YYYY')}</p>
+                        <p>{formData.selectedDate?.format('MMMM D, YYYY')}</p>
                     </div>
                     <div>
                         <AccessTimeIcon />
