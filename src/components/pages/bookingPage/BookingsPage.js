8b0733ca5a5fa1c7deff84b329f5105af90d61eb
unknown <syedahshifa@ymail.com>
2025-04-24 14:19:07 +0530

    Pass the props (errors, onInputBlur, onTimeFocus) to Bookingform component

--- src/components/pages/bookingPage/BookingsPage.js
+++ src/components/pages/bookingPage/BookingsPage.js
@@ -2,7 +2,7 @@ import React from 'react'
 import BookingForm from './BookingForm'
 import styles from '../bookingPage/bookingsPage.module.css'
 
-function BookingsPage({availableTimes, formData, onDateChange, onInputChange, onClearForm, onSubmitForm}) {
+function BookingsPage({availableTimes, formData, errors, onInputBlur, onTimeFocus, onDateChange, onInputChange, onClearForm, onSubmitForm}) {
   return (
     <>
       <div className={styles.imgContainer} role="banner" aria-labelledby="reservation-heading">
@@ -10,7 +10,13 @@ function BookingsPage({availableTimes, formData, onDateChange, onInputChange, on
         <p aria-labelledby="book-table-heading">Book Your Table Online Now!</p>
       </div>
       <section>
-        <BookingForm availableTimes={availableTimes} formData={formData} onDateChange={onDateChange}
+        <BookingForm 
+          availableTimes={availableTimes} 
+          formData={formData} 
+          errors={errors} 
+          onInputBlur={onInputBlur}
+          onDateChange={onDateChange}
+          onTimeFocus={onTimeFocus}
           onInputChange={onInputChange}
           onClearForm={onClearForm}
           onSubmitForm={onSubmitForm}/>
